c349aa3cb0bb2a210e9d9067b780bc9fbaf7d6c8
gold11997 <abrahambenson90@gmail.com>
2023-10-05 13:10:59 +0100

    Type Checking

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Use mypy to validate the piece of code and apply any changes."""
+from typing import Tuple, List
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """Returns a list of integers multiplied by certain factor."""
